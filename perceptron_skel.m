95af982eaa942a17c5373d03f09ad6fb69a1a580
jeffreyjohnens <jeffreyjohnens@gmail.com>
2015-11-17 12:04:59 -0800

    Added Dataset parser

--- perceptron_skel.m
+++ perceptron_skel.m
@@ -1,16 +1,16 @@
 % HMM PERCEPTRON
 
 MAX_EPOCH = 100;
+[X,T] = parseDataset('jsbach_chorals_harmony.data');
 
 % initialize weights to zero
-% weights used at every vertical evaluation and horizontal evaluation
+% weights used at every vertical evalu'ation and horizontal evaluation
 W = zeros(43,1); % 43 basis functions used in paper
 S = 43; % number of basis functions
 K = 24; % number of labels
-N = 50; % number of training examples 
+N = size(X,2); % number of training examples
 
 % mock random values
-Xt = zeros(N:1);
 Yt = ones(20:1);
 PHI = randn(S,K);
 W = randn(S,1);
@@ -20,7 +20,7 @@ for epoch=1:MAX_EPOCH
     rp = randperm(size(Xt, 1));
     for n_i=1:N % for each training example
         % calculate H(X) for n_ith training example in Xt
-        T = 20; % number of events in n_ith training example (will vary)
+        T = X(n_i).numEvents; % number of events in n_ith training example (will vary)
         r = zeros(T,K); % results of basis functions
         for t=1:T % for each event
             for k=1:K % for each label
