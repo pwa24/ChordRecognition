6d1ac703db694eb7b102404b407efb9590686156
jeffreyjohnens <jeffreyjohnens@gmail.com>
2015-11-17 11:37:48 -0800

    Create perceptron_skel.m

---
+++ perceptron_skel.m
@@ -0,0 +1,44 @@
+% HMM PERCEPTRON
+
+MAX_EPOCH = 100;
+
+% initialize weights to zero
+% weights used at every vertical evaluation and horizontal evaluation
+W = zeros(43,1); % 43 basis functions used in paper
+S = 43; % number of basis functions
+K = 24; % number of labels
+N = 50; % number of training examples 
+
+% mock random values
+Xt = zeros(N:1);
+Yt = ones(20:1);
+PHI = randn(S,K);
+W = randn(S,1);
+I = 2;
+    
+for epoch=1:MAX_EPOCH
+    rp = randperm(size(Xt, 1));
+    for n_i=1:N % for each training example
+        % calculate H(X) for n_ith training example in Xt
+        T = 20; % number of events in n_ith training example (will vary)
+        r = zeros(T,K); % results of basis functions
+        for t=1:T % for each event
+            for k=1:K % for each label
+                for s=1:S % for each basis function
+                    r(t,k) = r(t,k) + W(s) * PHI(s,k);
+                end
+            end
+        end
+        [no,Hx] = max(r'); % get arg max for each event
+        
+        if (isequal(Hx,Yt) == 0) % compare estimate (Hx) with target values
+            for s=1:S % for each basis function
+                A = 0;
+                for t=1:T % for each event
+                    A = PHI(s,k) * I;
+                end
+                W(s) = W(s) + A;
+            end
+        end
+    end
+end
