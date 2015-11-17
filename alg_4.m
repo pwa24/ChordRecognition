5c15789256b47388a153a9e3126b0a4b866cdfce
jeffreyjohnens <jeffreyjohnens@gmail.com>
2015-11-17 11:42:37 -0800

    NEED THIS TO RUN CARPE DIEM

---
+++ alg_4.m
@@ -0,0 +1,43 @@
+function alg_4(t, yt, B, Order, S0, S1, G, yh, yp, K)
+
+yh(t-1) = Order(1,t-1);
+yp(t-1) = Order(2,t-1);
+in = 3; % index for algorithm 4;
+
+% vertex is open if the reward of its best incoming path has been computed
+% i.e. G is non-zero
+
+%fprintf('%d %d\n', yp(t-1), t);
+
+while( G(yp(t-1),t-1) > 0 )
+    
+    if ( G(yh(t-1),t-1) + S1(yh(t-1),yt,t-1) < G(yp(t-1),t-1) + S1(yp(t-1),yt,t-1) )
+        yh(t-1) = yp(t-1); 
+    end
+    
+    if (in > K)
+        break;
+    end
+    yp(t-1) = Order(in,t-1);
+    in = in + 1;
+    
+end
+
+while ( G(yh(t-1),t-1) + S1(yh(t-1),yt,t-1) < B(t-1) + S0(yp(t-1),t-1) ) % + max(S1(yh(t)), S1(yh(t-1)))
+
+    % call algorithm 4 on yp(t-1)
+    alg_4(t-1,yp(t-1),B,Order,S0,S1,G,yh,yp,K);
+    
+    if (in > K)
+        break;
+    end
+    if ( G(yh(t-1),t-1) + S1(yh(t-1),yt,t-1) < G(yp(t-1),t-1) + S1(yp(t-1),yt,t-1) )
+        yh(t-1) = yp(t-1); 
+    end
+    yp(t-1) = Order(in,t-1);
+    in = in + 1;
+end
+
+G(yt,t) = G(yh(t-1),t-1) + S1(yh(t-1),yt,t-1) + S0(yt,t);
+
+
