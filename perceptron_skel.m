32868bdcf3be84766f0ee2d5c402116484c62d66
jeffreyjohnens <jeffreyjohnens@gmail.com>
2015-11-19 00:21:02 -0800

    Updated to work with basis.m

--- perceptron_skel.m
+++ perceptron_skel.m
@@ -6,38 +6,40 @@ MAX_EPOCH = 100;
 % initialize weights to zero
 % weights used at every vertical evalu'ation and horizontal evaluation
 W = zeros(43,1); % 43 basis functions used in paper
-S = 43; % number of basis functions
-K = 24; % number of labels
+K = 144; % number of labels
 N = size(X,2); % number of training examples
-
-% mock random values
-Yt = ones(20:1);
-PHI = randn(S,K);
-W = randn(S,1);
-I = 2;
     
 for epoch=1:MAX_EPOCH
-    rp = randperm(size(Xt, 1));
+    fprintf('EPOCH %d\n',epoch);
+    %rp = randperm(size(Xt, 1));
     for n_i=1:N % for each training example
-        % calculate H(X) for n_ith training example in Xt
-        T = X(n_i).numEvents; % number of events in n_ith training example (will vary)
-        r = zeros(T,K); % results of basis functions
-        for t=1:T % for each event
-            for k=1:K % for each label
-                for s=1:S % for each basis function
-                    r(t,k) = r(t,k) + W(s) * PHI(s,k);
-                end
-            end
-        end
-        [no,Hx] = max(r'); % get arg max for each event
+        TE = X(n_i).numEvents; % number of events in n_ith training example (will vary)
+        Hx = zeros(TE,1); % estimated labels
+        F = zeros(43,TE);
         
-        if (isequal(Hx,Yt) == 0) % compare estimate (Hx) with target values
-            for s=1:S % for each basis function
-                A = 0;
-                for t=1:T % for each event
-                    A = PHI(s,k) * I;
+        for t=1:TE % for each event
+            r = zeros(K,1); % results of basis functions
+            for k=1:K % for each label
+                if (t > 1)
+                    F(:,t) = basis(X(n_i),k,Hx(t-1),t);
+                else
+                    F(:,t) = basis(X(n_i),k,0,t);
                 end
-                W(s) = W(s) + A;
+                r(k) = sum(W' * F(:,t));
+            end
+            [no, IN] = max(r);
+            Hx(t) = IN;
+        end
+        
+        if (isequal(Hx,T(n_i).chord(:,4)) == 0) % compare estimate (Hx) with target values
+            for s=1:43
+                for t=1:TE
+                    if (Hx(t) == T(n_i).chord(t,4))
+                        W(s) = W(s) + F(s,t);
+                    else
+                        W(s) = W(s) - F(s,t);
+                    end
+                end
             end
         end
     end
