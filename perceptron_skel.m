71351fc7a97db3d631d37fe32606f20bd518bd04
jeffreyjohnens <jeffreyjohnens@gmail.com>
2015-11-20 15:44:21 -0800

    current version

--- perceptron_skel.m
+++ perceptron_skel.m
@@ -6,50 +6,27 @@ ETA = 1;
 
 % initialize weights to zero
 % weights used at every vertical evalu'ation and horizontal evaluation
-W = zeros(43,1); % 43 basis functions used in paper
+W = randn(43,1); % 43 basis functions used in paper
 K = 144; % number of labels
-N = 1;%size(X,2); % number of training examples
+N = 6;%size(X,2); % number of training examples
     
 for epoch=1:MAX_EPOCH
-    fprintf('EPOCH %d\n',epoch);
+    fprintf('EPOCH %d CORRECT %d\n',epoch, CORRECT);
     %rp = randperm(size(Xt, 1));
     for n_i=1:N % for each training example
         TE = X(n_i).numEvents; % number of events in n_ith training example (will vary)
         Hx = zeros(TE,1); % estimated labels
         F = zeros(43,TE,K);
         
-        for t=1:TE % for each event
-            RES = zeros(K,1); % results of basis functions
-            for k=0:K-1 % for each label
-                if (t > 1)
-                    F(:,t,k+1) = basis(X(n_i),k,Hx(t-1),t);
-                else
-                    F(:,t,k+1) = basis(X(n_i),k,0,t);
-                end
-                RES(k+1) = sum(W' * F(:,t,k+1));
-            end
-            % testing stuff for me
-            [no, IN] = min(RES);
-            [no, ON] = max(RES);
-            if (IN > 1)
-                CBOTH = [IN,T(n_i).chord(t), RES(IN), RES(IN-1)];
-            end
-            % testing stuff for me
-            Hx(t) = IN;
-        end
-        
-        % testing stuff for me
-        CORRECT = sum(Hx - T(n_i).chord==0);
-        COMP = [mod(Hx,12),mod(T(n_i).chord,12), Hx, T(n_i).chord];
-        % testing stuff for me
+        Hx = carpe_diem_alg(X(n_i),W,K,TE);
         
         if (isequal(Hx,T(n_i).chord) == 0) % compare estimate (Hx) with target values
             for s=1:43
                 for t=1:TE
                     if (Hx(t) == T(n_i).chord(t))
-                        W(s) = W(s) + ETA * F(s,t,Hx(t));
+                        W(s) = W(s) + ETA * F(s,t,Hx(t)+1);
                     else
-                        W(s) = W(s) - ETA * F(s,t,Hx(t));
+                        W(s) = W(s) - ETA * F(s,t,Hx(t)+1);
                     end
                 end
             end
