117e60c9edc4f38ad99fc253cf10c7998e7c83dd
jeffreyjohnens <jeffreyjohnens@gmail.com>
2015-11-20 16:41:17 -0800

    CURRENT VERSION

--- alg_4.m
+++ alg_4.m
@@ -7,8 +7,6 @@ in = 3; % index for algorithm 4;
 % vertex is open if the reward of its best incoming path has been computed
 % i.e. G is non-zero
 
-%fprintf('%d %d\n', yp(t-1), t);
-
 S1(yh(t-1),yt,t-1) = W(22:43)'* basis(X,yh(t-1),yt,t-1,1);
 S1(yp(t-1),yt,t-1) = W(22:43)'* basis(X,yp(t-1),yt,t-1,1);
 
@@ -29,7 +27,9 @@ end
 while ( G(yh(t-1),t-1) + S1(yh(t-1),yt,t-1) < B(t-1) + S0(yp(t-1),t-1) ) % + max(S1(yh(t)), S1(yh(t-1)))
 
     % call algorithm 4 on yp(t-1)
-    alg_4(t-1,yp(t-1),B,Order,S0,S1,G,yh,yp,K,X,W);
+    if (t-1 > 1)
+        alg_4(t-1,yp(t-1),B,Order,S0,S1,G,yh,yp,K,X,W);
+    end
     
     if (in > K)
         break;
@@ -43,4 +43,4 @@ end
 
 G(yt,t) = G(yh(t-1),t-1) + S1(yh(t-1),yt,t-1) + S0(yt,t);
 
-
+end
