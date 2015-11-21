fa9265ea3855a28828dd78a3ed89d51ef9d4804c
jeffreyjohnens <jeffreyjohnens@gmail.com>
2015-11-20 16:10:54 -0800

    CURRENT VERSION

--- alg_4.m
+++ alg_4.m
@@ -1,4 +1,4 @@
-function alg_4(t, yt, B, Order, S0, S1, G, yh, yp, K)
+function alg_4(t, yt, B, Order, S0, S1, G, yh, yp, K, X, W)
 
 yh(t-1) = Order(1,t-1);
 yp(t-1) = Order(2,t-1);
@@ -8,6 +8,9 @@ in = 3; % index for algorithm 4;
 % i.e. G is non-zero
 
 %fprintf('%d %d\n', yp(t-1), t);
+
+S1(yh(t-1),yt,t-1) = W(22:43)'* basis(X,yh(t-1),yt,t-1,1);
+S1(yp(t-1),yt,t-1) = W(22:43)'* basis(X,yp(t-1),yt,t-1,1);
 
 while( G(yp(t-1),t-1) > 0 )
     
@@ -26,7 +29,7 @@ end
 while ( G(yh(t-1),t-1) + S1(yh(t-1),yt,t-1) < B(t-1) + S0(yp(t-1),t-1) ) % + max(S1(yh(t)), S1(yh(t-1)))
 
     % call algorithm 4 on yp(t-1)
-    alg_4(t-1,yp(t-1),B,Order,S0,S1,G,yh,yp,K);
+    alg_4(t-1,yp(t-1),B,Order,S0,S1,G,yh,yp,K,X,W);
     
     if (in > K)
         break;
