85962faeecba5d7ef11d98412d85685a8961b910
Patrick Wang <patrickwang10@gmail.com>
2015-11-22 22:24:05 -0800

    Fixed some bugs with MIDI converter. Added ocanada dataset.
    Not implementing Meter at the moment.

--- Datasets/convert.py
+++ Datasets/convert.py
@@ -3,6 +3,15 @@ import csv
 import subprocess
 import os
 
+#debuging use
+def visualize(notes):
+    key = ['C','C#','D','Eb','E','F','F#','G','Ab','A','Bb','B']
+    pitches = notes[0:11]
+    newNotes = ['', '', '', '', '', '', '', '', '', '', '', '', key[notes[12]]]
+    for i in range(0,11):
+        if pitches[i] == 1: newNotes[i] = key[i]
+    return newNotes
+
 #notes = list of midi pitches (0-127)
 #newNotes = [b, b, b, b, b, b, b, b, b, b, b, b, bass]
 def formatNotes(notes):
@@ -19,59 +28,76 @@ def readcsv(filePath, sid):
     with open(filePath, newline='') as file:
         #CSV -> List
         RR = list(csv.reader(file, delimiter=',', skipinitialspace=True))
-        #Convert first 2 columns to ints
-        R = list(map(lambda x: [int(x[0]), int(x[1])] + x[2:], RR))
-        #Sort by time, then by type
-        R = sorted(R, key=(lambda x: (x[1], x[2])))
+        #Keep only 'Note_<off/on>_c' rows
+        R = list(filter(lambda x: (x[2] == 'Note_on_c') or (x[2] == 'Note_off_c'), RR))
+        #Keep only columns 2, 3, 5, 6
+        R = list(map(lambda x: [int(x[1]), x[2], int(x[4]), int(x[5])], R))
+        #Sort by time, then by velocity
+        R = sorted(R, key=(lambda x: (x[0], x[3])))
         eList = dict()
         heldNotes = []
         prevTime = 0
+        hold = False
         for row in R:
-            if (row[2] == 'Note_off_c' or (row[2] == 'Note_on_c' and row[5] == '0')):
-                if (prevTime != row[1]):
-                    currNotes = []
-                    currNotes.extend(heldNotes) #shallow copy
-                    eList.update(dict({prevTime:currNotes}))
-                offNote = int(row[4])
-                if offNote in heldNotes: heldNotes.remove(offNote)
-                prevTime = row[1]
-            if (row[2] == 'Note_on_c' and row[5] != '0'):
-                onNote = int(row[4])
-                heldNotes.append(onNote)
-                prevTime = row[1]
+            if (prevTime != row[0] and hold == True):
+                #Play heldNotes
+                currNotes = []
+                currNotes.extend(heldNotes) #shallow copy
+#                print(currNotes)                   #DEBUG
+                eList.update(dict({prevTime:currNotes}))
+            if (row[1] == 'Note_off_c' or (row[1] == 'Note_on_c' and row[3] == 0)):
+                if row[2] in heldNotes: heldNotes.remove(row[2])
+                hold = False
+                prevTime = row[0]
+            elif (row[1] == 'Note_on_c'):
+                heldNotes.append(row[2])
+                hold = True
+                prevTime = row[0]
+#            print(row)                             #DEBUG
         eList2 = []
         i = 1
         W = []
         for k, v in sorted(eList.items()):
+#            x = [sid, k]                           #DEBUG
+#            x.extend(visualize(formatNotes(v)))    #DEBUG
             x = [sid, i]
             x.extend(formatNotes(v))
             W.append(x)
             i = i + 1
         return W
 
-def writecsv(filePath, W):
-    with open(filePath, 'a', newline='') as file:
+def writecsv(filePath, mode, W):
+    with open(filePath, mode, newline='') as file:
         writer = csv.writer(file)
         writer.writerows(W)
         return
 
 parser = argparse.ArgumentParser()
-parser.add_argument("inputDir")
-parser.add_argument("outputDir")
+parser.add_argument('inputPath', help='MIDI file or Dir containing MIDI files')
+parser.add_argument('outputFile', help='Output csv file')
 args = parser.parse_args()
-
-inputFolder = args.inputDir + '/'
-outputFolder = args.outputDir + '/'
-
-for file in sorted(os.listdir(inputFolder)):
-    if file.endswith('.mid'):
-        fileTitle = file.split(sep='.')[0]
-        midiToCSV = ['Midicsv.exe', inputFolder + file, 'temp.csv']
+#convert single file
+if os.path.isfile(args.inputPath):
+    if args.inputPath.endswith('.mid'):
+        fileTitle = args.inputPath.split(sep='.')[0]
+        midiToCSV = ['Midicsv.exe', args.inputPath, 'temp.csv']
         subprocess.call(midiToCSV)
         W = readcsv('temp.csv', fileTitle)
-        writecsv(outputFolder + 'X.csv', W)
-subprocess.call('rm temp.csv')
-
-
+        writecsv(args.outputFile, 'w', W)
+    subprocess.call('rm temp.csv')
+#convert multiple files
+elif os.path.isdir(args.inputPath):
+    #remove outputFile if exists
+    if os.path.isfile(args.outputFile): subprocess.call('rm ' + args.outputFile)
+    for file in sorted(os.listdir(args.inputPath)):
+        if file.endswith('.mid'):
+            fileTitle = file.split(sep='.')[0]
+            midiToCSV = ['Midicsv.exe', args.inputPath + '/' + file, 'temp.csv']
+            subprocess.call(midiToCSV)
+            W = readcsv('temp.csv', fileTitle)
+            writecsv(args.outputFile, 'a', W)
+    subprocess.call('rm temp.csv')
+else:
+    print('Invalid input path')
 
 
